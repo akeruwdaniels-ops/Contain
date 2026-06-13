d8b14aaa5927325f5aa8eb7441a77e6670752fe7
akeruwdaniels-ops <akeruwdaniels@gmail.com>
2026-06-13 20:24:45 +0300

    Update deriv_rb_bot_mc.py

--- deriv_rb_bot_mc.py
+++ deriv_rb_bot_mc.py
@@ -66,7 +66,7 @@ CONFIG = {
     "api_token" : os.environ.get("DERIV_API_TOKEN", ""),
 
     # -- Symbol ----------------------------------------------------------------
-    "symbol"    : "RDBEAR",
+    "symbol"    : "1HZ10V",
 
     # -- Tick collection -------------------------------------------------------
     "collect_hours" : 0.6,          # 30 min history
